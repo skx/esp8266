ce332c432a89bdbdb216c15a5a8c1207247ac0e1
Steve Kemp <steve@steve.org.uk>
2017-02-25 11:43:53 +0200

    Updated project significantly.
    * We now use WiFi Manager.
    * We allow over the air updates.
    * Our measurement is cleaner, with fewer global variables.

--- d1-temp/d1-temp.ino
+++ d1-temp/d1-temp.ino
@@ -1,29 +1,69 @@
+
 #include <ESP8266WiFi.h>
+#include <ArduinoOTA.h>
 #include <ESP8266HTTPClient.h>
 
+//
+// DHT11 / DHT22 sensor interface
+//
 #include "dht.h"
 
+//
+// The pin we're connecting the sensor to
+//
 #define DHTPIN D2
 
+//
+// Use the user-friendly WiFI library?
+//
+// If you don't want to use this then comment out the following line:
+//
+#define WIFI_MANAGER
+
+//
+//  Otherwise define a SSID / Password
+//
+#ifdef WIFI_MANAGER
+# include "WiFiManager.h"
+#else
+# define WIFI_SSID "SCOTLAND"
+# define WIFI_PASS "highlander1"
+#endif
+
+//
+// The name of this project.
+//
+// Used for:
+//   Access-Point name, in config-mode
+//   OTA name.
+//
+#define PROJECT_NAME "D1-TEMP"
+
+
+//
+// Should we enable debugging (via serial-console output) ?
+//
+// Use either `#undef DEBUG`, or `#define DEBUG`.
+//
+#define DEBUG
+
+
+//
+// If we did then DEBUG_LOG will log a string, otherwise
+// it will be ignored as a comment.
+//
+#ifdef DEBUG
+#  define DEBUG_LOG(x) Serial.print(x)
+#else
+#  define DEBUG_LOG(x)
+#endif
+
+
+
 #define SERVER_ADDRESS "192.168.10.64"
-#define SERVER_PORT 80
+#define SERVER_PORT    80
+#define SERVER_SCRIPT  "/cgi-bin/temp.cgi"
 
-dht DHT;
-
-float humidity = 0;
-float temperature = 0;
-
-const char* ssid = "SCOTLAND";
-const char* password = "highlander1";
-
-int sendData(String url, String payload)
-{
-    HTTPClient http;
-    http.begin(SERVER_ADDRESS, SERVER_PORT, url);
-    http.addHeader("Content-Type", "application/json");
-    http.POST(payload);
-    http.end();
-}
 
 void measureDHT()
 {
@@ -31,6 +71,8 @@ void measureDHT()
     //
     // Make a reading
     //
+    dht DHT;
+
     int chk = DHT.read22(DHTPIN);
 
     //
@@ -47,7 +89,14 @@ void measureDHT()
 
         // Send it away
         String payload = "{\"temperature\":" + String(DHT.temperature) + ",\"humidity\":" + String(DHT.humidity) + "}";
-        sendData("/cgi-bin/temp.cgi", payload);
+
+        HTTPClient http;
+        http.begin(SERVER_ADDRESS, SERVER_PORT, SERVER_SCRIPT);
+        http.addHeader("Content-Type", "application/json");
+        http.POST(payload);
+        http.end();
+
+        return;
 
     }
     else
@@ -89,26 +138,115 @@ void measureDHT()
 
 }
 
+
+//
+// Called once, when the device boots.
+//
 void setup()
 {
+    //
+    // Setup the serial-console.
+    //
     Serial.begin(115200);
 
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
+    //
+    // Handle WiFi setup
+    //
+#ifdef WIFI_MANAGER
 
-    while (WiFi.waitForConnectResult() != WL_CONNECTED)
+    WiFiManager wifiManager;
+    wifiManager.autoConnect(PROJECT_NAME);
+
+#else
+    //
+    // Connect to the WiFi network, and set a sane
+    // hostname so we can ping it by name.
+    //
+    WiFi.mode(WIFI_STA);
+    WiFi.hostname(PROJECT_NAME);
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+    //
+    // Show that we're connecting to the WiFi.
+    //
+    DEBUG_LOG("WiFi connecting: ");
+
+    //
+    // Try to connect to WiFi, constantly.
+    //
+    while (WiFi.status() != WL_CONNECTED)
     {
-        Serial.println("Connection Failed! Rebooting...");
-        delay(5000);
-        ESP.restart();
+        DEBUG_LOG(".");
+        delay(500);
     }
 
-    Serial.println("Ready");
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
+
+#endif
+    //
+    // Now we're connected show the local IP address.
+    //
+    DEBUG_LOG("\nWiFi connected ");
+    DEBUG_LOG(WiFi.localIP());
+    DEBUG_LOG("\n");
+
+    //
+    // The final step is to allow over the air updates
+    //
+    // This is documented here:
+    //     https://randomnerdtutorials.com/esp8266-ota-updates-with-arduino-ide-over-the-air/
+    //
+    // Hostname defaults to esp8266-[ChipID]
+    //
+    ArduinoOTA.setHostname(PROJECT_NAME);
+
+    ArduinoOTA.onStart([]()
+    {
+        DEBUG_LOG("OTA Start\n");
+    });
+    ArduinoOTA.onEnd([]()
+    {
+        DEBUG_LOG("OTA End\n");
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
+    {
+        char buf[16];
+        memset(buf, '\0', sizeof(buf));
+        snprintf(buf, sizeof(buf) - 1, "Upgrade - %02u%%          ", (progress / (total / 100)));
+        DEBUG_LOG(buf);
+        DEBUG_LOG("\n");
+    });
+    ArduinoOTA.onError([](ota_error_t error)
+    {
+        DEBUG_LOG("Error - ");
+        DEBUG_LOG(error);
+        DEBUG_LOG(" ");
+
+        if (error == OTA_AUTH_ERROR)
+            DEBUG_LOG("Auth Failed\n");
+        else if (error == OTA_BEGIN_ERROR)
+            DEBUG_LOG("Begin Failed\n");
+        else if (error == OTA_CONNECT_ERROR)
+            DEBUG_LOG("Connect Failed\n");
+        else if (error == OTA_RECEIVE_ERROR)
+            DEBUG_LOG("Receive Failed\n");
+        else if (error == OTA_END_ERROR)
+            DEBUG_LOG("End Failed\n");
+    });
+
+    //
+    // Ensure the OTA process is running & listening.
+    //
+    ArduinoOTA.begin();
+
 
 }
 
+
+//
+// Called constantly.
+//
+// Read the value of the temperature-sensor, and submit it, once a minute.
+//
 void loop()
 {
     measureDHT();
