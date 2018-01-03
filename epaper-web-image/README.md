75d1d36676540bdf813658e692f2af2c302043a1
Steve Kemp <steve@steve.org.uk>
2018-01-03 19:28:08 +0200

    Initial import

---
+++ epaper-web-image/README.md
@@ -0,0 +1,44 @@
+# epaper-web-image
+
+This directory contains code to retrieve an image over HTTP/HTTPS and display
+it upon a 4.2" epaper display.
+
+
+## How it works
+
+The code works indirectly, rather than making a simple request for a JPG, a PNG,
+or some other known-image format it instead fetches a file that contains drawing
+data.
+
+To display a specific image you need to process it, to create a data-file.
+
+The supplied files beneath [util/](util/) do that.  In the example we
+fetch the file:
+
+* knot.dat
+
+That was created via:
+
+    ./utils/export ./knot.jpg > knot.dat
+
+
+## Required Libraries
+
+To make this example complete you'll need to install two libraries:
+
+* https://github.com/ZinggJM/GxEPD
+* https://github.com/adafruit/Adafruit-GFX-Library
+
+Installation instructions are outside the scope of this document.
+
+
+## Further Reading
+
+For details of wiring, and an overview of the whole process please see the
+following writeup:
+
+* https://steve.fi/Hardware/d1-epaper/
+
+
+Steve
+-- 
---
+++ epaper-web-image/epaper-web-image.ino
@@ -0,0 +1,350 @@
+//
+// Display an image fetched via a remote URL.
+//
+// This script is designed to display a pre-processed image upon a 4.2"
+// epaper, and is based upon the writeup located here:
+//
+//  https://steve.fi/Hardware/d1-epaper/
+//
+// To use this you'll need to:
+//
+//   1.  Find a monochrome image which is 400x300 pixels in size.
+//
+//   2.  Process it into a series of lines, via the "export.pl" script.
+//
+//   3.  Upload the result to a remote HTTP-server.
+//
+//   4.  Point this script at the URL to that file.
+//
+//   5.  Profit.
+//
+// Steve
+// --
+//
+
+
+
+//
+// ePaper library.
+//
+#include <GxEPD.h>
+#include <GxGDEW042T2/GxGDEW042T2.cpp>      // 4.2" b/w
+#include <GxIO/GxIO_SPI/GxIO_SPI.cpp>
+#include <GxIO/GxIO.cpp>
+
+
+//
+// WiFi manager library, for working as an access-point, etc.
+//
+#include "WiFiManager.h"
+
+
+//
+// WiFi & over the air updates
+//
+#include <ESP8266WiFi.h>
+#include <ArduinoOTA.h>
+
+//
+// Headers for SSL/MAC access.
+//
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecure.h>
+
+
+//
+// Debug messages over the serial console.
+//
+#include "debug.h"
+
+
+//
+// The name of this project.
+//
+// Used for the Access-Point name, and for OTA-identification.
+//
+#define PROJECT_NAME "EPAPER-IMAGE"
+
+
+//
+// The helper & object for the epaper display.
+//
+GxIO_Class io(SPI, SS, 0, 2);
+GxEPD_Class display(io);
+
+
+//
+// Setup, which is called once.
+//
+void setup()
+{
+    //
+    // Setup serial-console & the display.
+    //
+    Serial.begin(115200);
+    display.init();
+
+    //
+    // Handle the WiFi connection.
+    //
+    WiFiManager wifiManager;
+    wifiManager.setAPCallback(access_point_callback);
+    wifiManager.autoConnect(PROJECT_NAME);
+}
+
+
+//
+// If we're unconfigured we run an access-point.
+//
+// Show that, explicitly.
+//
+void access_point_callback(WiFiManager* myWiFiManager)
+{
+    DEBUG_LOG("AccessPoint Mode Enabled.\n");
+}
+
+
+//
+// Fetch and display the image specified at the given URL
+//
+void display_url(const char *url)
+{
+    //
+    // Clear the display.
+    //
+    display.fillScreen(GxEPD_WHITE);
+    display.setTextColor(GxEPD_BLACK);
+    display.drawLine(0,0,400,400, GxEPD_BLACK);
+    display.drawLine(0,400,0,00, GxEPD_BLACK);
+      display.update();
+
+      
+    display.fillScreen(GxEPD_WHITE);
+    display.setTextColor(GxEPD_BLACK);
+    //
+    // The host and path we're going to fetch.
+    //
+    char m_host[50] = { '\0' };
+    char m_path[50] = { '\0' };
+
+    /*
+     * Look for the host.
+     */
+    char *host_start = strstr(url, "://");
+
+    if (host_start != NULL)
+    {
+        /*
+         * Look for the end.
+         */
+        host_start += 3;
+        char *host_end = host_start;
+
+        while (host_end[0] != '/')
+            host_end += 1;
+
+        strncpy(m_host, host_start, host_end - host_start);
+        strcpy(m_path, host_end);
+    }
+    else
+    {
+        Serial.println("BUG: Bogus URL");
+        return;
+    }
+
+     DEBUG_LOG( "Converted %s -> Host:'%s' Path:'%s'\n", url, m_host, m_path );
+     
+    /*
+     * Create the appropriate client-object.
+     */
+    WiFiClientSecure m_client;
+    int port = 443;
+
+    //
+    // Keep track of where we are.
+    //
+    bool finishedHeaders = false;
+    bool currentLineIsBlank = true;
+    String m_body = "";
+    long now;
+
+    DEBUG_LOG( "About to make the connection to '%s:%d'\n", m_host, port );
+
+    //
+    // Connect to the remote host, and send the HTTP request.
+    //
+    if (m_client.connect(m_host, port))
+    {
+        DEBUG_LOG( "Making HTTP-request\n" );
+        
+        m_client.print("GET ");
+        m_client.print(m_path);
+        m_client.println(" HTTP/1.0");
+        m_client.print("Host: ");
+        m_client.println(m_host);
+        m_client.println("User-Agent: epaper-web-image/1.0");
+        m_client.println("Connection: close");
+        m_client.println("");
+
+        DEBUG_LOG( "Made HTTP-request\n");
+        
+        now = millis();
+
+        //
+        // Test for timeout waiting for the first byte.
+        //
+        while (m_client.available() == 0)
+        {
+            if (millis() - now > 15000)
+            {
+                Serial.println(">>> Client Timeout !");
+                m_client.stop();
+                return;
+            }
+        }
+
+        //
+        // Now we hope we'll have smooth-sailing, and we'll
+        // read a single character until we've got it all
+        //
+        while (m_client.available())
+        {
+            char c = m_client.read();
+
+            if (finishedHeaders)
+            {
+              static bool x = false;
+                //
+                // Here we're reading the body of the response.
+                //
+                // We keep appending characters, but we don't want to
+                // store the whole damn response in a string, because
+                // that would eat all our RAM.
+                //
+                // Instead we read each character until we find a newline
+                //
+                // Once we find a newline we process that input, then
+                // we keep reading more.
+                //
+                m_body = m_body + c;
+
+                if (c == '\n')
+                {
+if ( x == false ) {
+                      DEBUG_LOG( "READ LINE: '%s'", m_body.c_str() );
+                      //DEBUG_LOG( "%d -> %d -> %d -> %d\n", line[0], line[1], line[2], line[3]);
+                    x = true;
+                    }
+
+                    
+                    //
+                    // Here we've read a complete line.
+                    //
+                    // The line will be of the form "x,y,X,Y", so we
+                    // need to parse that into four integers, and then
+                    // draw the appropriate line on our display.
+                    //
+                    int line[3] = { 0 };
+                    int i = 0;
+                    
+
+                    //
+                    // Parse into values.
+                    // 
+                    char *tmp = strdup( m_body.c_str() );
+                    
+
+                    char* ptr = strtok(tmp, ",");
+
+                    while(ptr != NULL) {
+                      // create next part
+                      line[i] = atoi( ptr );
+                      i++;  
+                      ptr = strtok(NULL, ",");
+                    }
+                    free( tmp );
+                    
+                    //
+                    // Draw the line.
+                    //
+                    display.drawLine(line[0], line[1], line[2], line[3], GxEPD_BLACK);
+
+                    //
+                    // Now we nuke the memory so that we can read
+                    // another line.
+                    //
+                    m_body = "";
+                }
+            }
+            else
+            {
+                if (currentLineIsBlank && c == '\n')
+                    finishedHeaders = true;
+            }
+
+            if (c == '\n')
+                currentLineIsBlank = true;
+            else if (c != '\r')
+                currentLineIsBlank = false;
+
+            // Wait for more packetses
+            delay(1);
+        }
+
+        m_client.stop();
+
+        
+    //
+    // Trigger the update of the display.
+    //
+    display.update();
+    }
+    else
+    {
+      DEBUG_LOG( "Failed to connect\n");
+    }
+
+}
+
+
+void loop()
+{
+    //
+    // Have we displayed the image?
+    //
+    static bool show = false;
+
+    //
+    // The time we last displayed the image.
+    static long displayed = 0;
+    
+    //
+    // If we've not shown the image, then do so.
+    //
+    if (show == false)
+    {
+
+        //
+        // Fetch / display
+        //
+        display_url("https://steve.fi/Hardware/d1-epaper/knot.dat");
+
+        //
+        // And never again.
+        //
+        show = true;
+        displayed = millis();
+    }
+
+
+    //
+    // Update the image every hour.
+    //
+    // Do this by pretending we've never updated, even though we have.
+    //
+    if (millis() - displayed > (60 * 60 * 1000))
+        show = false;
+
+
+}
