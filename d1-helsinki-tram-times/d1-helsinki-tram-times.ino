3719635396ee1de8a89453bd41e772a2bb0f0e00
Steve Kemp <steve@steve.org.uk>
2018-01-07 11:08:05 +0200

    Always load jQuery with SSL.
    Also setup the cross-origin flag, and the integrity hash.

--- d1-helsinki-tram-times/d1-helsinki-tram-times.ino
+++ d1-helsinki-tram-times/d1-helsinki-tram-times.ino
@@ -880,7 +880,7 @@ void serveHTML(WiFiClient client)
     client.println("<meta charset=\"utf-8\">");
     client.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
     client.println("<link href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">");
-    client.println("<script src=\"//code.jquery.com/jquery-1.12.4.min.js\"></script>");
+    client.println("<script src=\"https://code.jquery.com/jquery-1.12.4.min.js\" integrity=\"sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU=\" crossorigin=\"anonymous\"></script>");
     client.println("<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>");
     client.println("</head>");
     client.println("<body>");
