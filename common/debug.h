e173b0a6905be25d7939152c12b5e8a386833dd1
Steve Kemp <steve@steve.org.uk>
2018-06-08 13:04:27 +0300

    Maintain a circular buffer of (20) debug-messages.
    This is useful for applications that might present a web-based
    user-interface, and want to see them.

--- common/debug.h
+++ common/debug.h
@@ -6,16 +6,32 @@
 #define DEBUG 1
 
 //
+// Last 20 debug messages.
+//
+#define DEBUG_MAX 20
+String debug_logs[DEBUG_MAX];
+
+//
 // Record a debug-message, only if `DEBUG` is defined.
 //
 void DEBUG_LOG(const char *format, ...)
 {
 #ifdef DEBUG
+    static int last_debug = 0;
     char buff[1024] = {'\0'};
     va_list arguments;
     va_start(arguments, format);
     vsnprintf(buff, sizeof(buff), format, arguments);
     Serial.print(buff);
+
+    debug_logs[last_debug] = String(buff);
+    last_debug += 1;
+
+    if (last_debug >= DEBUG_MAX)
+    {
+        last_debug = 0;
+    }
+
     va_end(arguments);
 #endif
 }
